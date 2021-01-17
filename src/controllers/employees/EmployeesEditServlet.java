24df07af63cba17759c4e1f6577c4dd92acf5966
yutaro.yanagi <yanatarou1129@yahoo.ne.jp>
2021-01-17 16:56:47 +0900

    Add Employee edit

---
+++ src/controllers/employees/EmployeesEditServlet.java
@@ -0,0 +1,48 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EmployeesEditServlet
+ */
+@WebServlet("/employees/edit")
+public class EmployeesEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesEditServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("employee", e);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("employee_id", e.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
