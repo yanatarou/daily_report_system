fccf00d47ff10a0d3e604995a66f9516dac40cb0
yutaro.yanagi <yanatarou1129@yahoo.ne.jp>
2021-01-17 16:53:49 +0900

    Add Employee show

---
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -0,0 +1,46 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EmployeesShowServlet
+ */
+@WebServlet("/employees/show")
+public class EmployeesShowServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesShowServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("employee", e);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+        rd.forward(request, response);
+    }
+
+}
